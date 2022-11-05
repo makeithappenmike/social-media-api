7c423857351b636ad9ce3501d3c512483f3fc86d
MakeithappenMike <omichaelaaron@gmail.com>
2022-11-05 12:54:55 -0400

    more seeding work

--- utils/data.js
+++ utils/data.js
@@ -1,23 +1,95 @@
-const usernames = [
-    'makeithappenmike'
+const users = [
+    {
+        'username': 'makeithappenmike',
+        'email': 'omichaelaaron@gmail.com',
+        'thoughts': [1],
+        'friends': [1, 2, 3]
+    },
+    {
+        'username': 'makeithappenmike2',
+        'email': 'omichaelaaron@gmail.com',
+        'thoughts': [2, 3],
+        'friends': [1, 2, 3]
+    }
 ];
   
 const thoughts = [
-    'This is a thought!',
+    {
+        'thoughtId': 1,
+        'thoughtText': 'This is a thought (1)!',
+        'createdAt': '01/01/2022',
+        'username': `makeithappenmike`,
+        'reactions': [1]
+    },
+    {
+        'thoughtId': 2,
+        'thoughtText': 'This is a thought (2)!',
+        'createdAt': '01/02/2022',
+        'username': `makeithappenmike2`,
+        'reactions': [2]
+    },
+    {
+        'thoughtId': 3,
+        'thoughtText': 'This is a thought (3)!',
+        'createdAt': '01/03/2022',
+        'username': `makeithappenmike2`,
+        'reactions': [3]
+    }
 ];
 
 const reactions = [
-    'This is a reaction!',
+    {
+        'reactionId': 1,
+        'reactionBody': 'This is a reaction (1)!',
+        'username': 'makeithappenmike',
+        'createdAt': '01/01/2022',
+    },
+    {
+        'reactionId': 2,
+        'reactionBody': 'This is a reaction (2)!',
+        'username': 'makeithappenmike2',
+        'createdAt': '01/02/2022',
+    },
+    {
+        'reactionId': 3,
+        'reactionBody': 'This is a reaction (3)!',
+        'username': 'makeithappenmike2',
+        'createdAt': '01/03/2022',
+    }
 ];
   
 // Get all usernames
-const getUsernames = () => `Usernames: ${usernames}`;
+const getUsernames = () => {
+    const usernameArray = [];
+    users.forEach((user) => {
+        console.log(user.username);
+        usernameArray.push(user.username);
+    });
+    console.log("Usernames:", usernameArray);
+    return usernameArray;
+}; getUsernames();
 
-// Get all usernames
-const getThoughts = () => `Thoughts: ${thoughts}`;
+// Get all thoughts
+const getThoughts = () => {
+    const thoughtArray = [];
+    thoughts.forEach((thought) => {
+        console.log(thought.thoughtId);
+        thoughtArray.push(thought.thoughtId);
+    });
+    console.log("Thoughts:", thoughtArray);
+    return thoughtArray;
+}; getThoughts();
 
-// Get all usernames
-const getReactions = () => `Reactions: ${reactions}`;
+// Get all reactions
+const getReactions = () => {
+    const reactionArray = [];
+    reactions.forEach((reaction) => {
+        console.log(reaction.reactionId);
+        reactionArray.push(reaction.reactionId);
+    });
+    console.log("Reactions:", reactionArray);
+    return reactionArray;
+}; getReactions();
   
 // Export the functions for use in seed.js
 module.exports = { getUsernames, getThoughts, getReactions };
